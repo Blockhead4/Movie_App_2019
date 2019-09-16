801ae73fe2754bfdd3a435d0b348ab1861ffa92a
blockhead4 <blockhead4@naver.com>
2019-09-16 16:23:11 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+function Food({ favorite }) {
+  return <h1>I like {favorite}</h1>;
+}
+
 function App() {
-  return <div>Hello!!!</div>;
+  return (
+    <div>
+      <h1>Hello!!!</h1>
+      <Food favorite="kimchi" />
+      <Food favorite="ramen" />
+      <Food favorite="samgyubsal" />
+      <Food favorite="chukumi" />
+    </div>
+  );
 }
 
 export default App;
